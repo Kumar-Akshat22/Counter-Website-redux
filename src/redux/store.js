9cef99bec344516038af9a15943309e44c12204c
Kumar Akshat <kakshat247@gmail.com>
2024-01-20 22:22:19 +0530

    added store for redux directory

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit'
+import CounterSlice from './slices/CounterSlice'
+
+// A store is a collection of slices
+// It is also a centralized storage for the data
+export const store = configureStore({
+
+    reducer:{
+        counter : CounterSlice
+    },
+})
