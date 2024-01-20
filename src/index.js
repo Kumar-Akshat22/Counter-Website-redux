8ac4139edf9ad2162492ec4f34de39bb1ecfba55
Kumar Akshat <kakshat247@gmail.com>
2024-01-20 22:38:33 +0530

    added components folder and modified index.js file

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-    <Provider>
+    <Provider store={store}>
         <App />
     </Provider>
 );
